e99e1c8b9b3a372cb848f167c7e9c215e8a2479d
acalvom <andreac.3108@gmail.com>
2021-05-19 10:36:32 +0200

    Button to flip name with Hooks & useState

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Home = () => {
+    const handleClick = () => {
+        setName(name.split("").reverse().join(""));
+        setAge(age * (-1))
+    }
+
+    const [name, setName] = useState('Andrea');
+    const [age, setAge] = useState(25);
+
     return (
         <Fragment>
             <h1>Home</h1>
+            <hr/>
+            <button type="button" className="btn btn-primary" onClick={handleClick}>Click here</button>
+            <h2>{name} is {age} years old</h2>
         </Fragment>
     );
 };
