46afa4ca27a55babc2a3baa1c3a52acd5c247c76
acalvom <andreac.3108@gmail.com>
2021-05-24 09:33:16 +0200

    Delete blog entry: entry id as function parameter

--- src/components/EntriesList.jsx
+++ src/components/EntriesList.jsx
@@ -4,6 +4,10 @@ const EntriesList = (props) => {
     const blog = props.entries;
     const title = props.pageTitle;
 
+    const handleDeleteClick = (id) => {
+        console.log('Delete id ', id)
+    }
+
     return (
         <div>
             <h1> {title} </h1>
@@ -15,6 +19,9 @@ const EntriesList = (props) => {
                             <h5 className="card-title">{item.title}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">{item.author}</h6>
                             <p className="card-text">{item.content}</p>
+                            <button type="button" className="btn btn-outline-danger btn-sm"
+                                    onClick={() => handleDeleteClick(item.id)}>Delete
+                            </button>
                         </div>
                     </div>
                 )
