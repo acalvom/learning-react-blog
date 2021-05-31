980a8c6d0728520f6086ef369c216e4116084b43
acalvom <andreac.3108@gmail.com>
2021-05-31 14:01:48 +0200

    Refactor: Set game info card into a separate component

--- src/components/GameDealDetail.jsx
+++ src/components/GameDealDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useFetch from "../hooks/useFetch";
 import {useParams} from 'react-router-dom';
+import GameInfoCard from "./GameInfoCard";
 
 const GameDealDetail = () => {
     const {id} = useParams();
@@ -13,17 +14,7 @@ const GameDealDetail = () => {
             <div className="row">
                 <div className="col-sm">
                     {isPending && <div><h2>Fetching data...</h2></div>}
-                    {gameInfo &&
-                    <div className="card my-2">
-                        <img className="card-img-top" src={gameInfo.thumb} alt="Card image cap"/>
-                        <div className="card-body">
-                            <h5 className="card-title">{gameInfo.name}</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">Game ID: {gameInfo.gameID}</h6>
-                            <p className="card-text">PVP: {gameInfo.retailPrice} €</p>
-                            <p className="card-text">With deal applied: {gameInfo.salePrice} €</p>
-                        </div>
-                    </div>
-                    }
+                    {gameInfo && <GameInfoCard data={gameInfo}/>}
                     {fetchError && <div><h3>{fetchError}</h3></div>}
                 </div>
             </div>
---
+++ src/components/GameInfoCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const GameInfoCard = (props) => {
+    const game = props.data;
+    return (
+        <div className="card my-2">
+            <img className="card-img-top" src={game.thumb} alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">{game.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Game ID: {game.gameID}</h6>
+                <p className="card-text">PVP: {game.retailPrice} €</p>
+                <p className="card-text">With deal applied: {game.salePrice} €</p>
+            </div>
+        </div>
+    );
+};
+
+export default GameInfoCard;
