5cd6f9d70ce40577f23c049e16b253b13c04e5f6
acalvom <andreac.3108@gmail.com>
2021-06-01 08:58:16 +0200

    Reformat code

--- src/components/EntriesList.jsx
+++ src/components/EntriesList.jsx
@@ -6,7 +6,7 @@ const EntriesList = (props) => {
     const handleDeleteClick = props.delete;
 
     return (
-        <div>
+        <div className="col-sm">
             <h1> {title} </h1>
             <hr/>
             {
@@ -15,7 +15,6 @@ const EntriesList = (props) => {
                         <div className="card-body">
                             <h5 className="card-title">{item.title}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">{item.author}</h6>
-                            <p className="card-text">{item.content}</p>
                             <button type="button" className="btn btn-outline-danger btn-sm"
                                     onClick={() => handleDeleteClick(item.id)}>Delete
                             </button>
