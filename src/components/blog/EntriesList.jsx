79148573cf952d374e6328cec46754cd14850432
acalvom <andreac.3108@gmail.com>
2021-06-08 13:16:54 +0200

    DELETE an existing entry with async/await (method 2)

--- src/components/blog/EntriesList.jsx
+++ src/components/blog/EntriesList.jsx
@@ -5,21 +5,6 @@ const EntriesList = (props) => {
     const blog = props.entries;
     const title = props.pageTitle;
 
-    /* ---- Para hacerlo con async y await ----
-    const deleteData = async (id) => {
-        const response = await fetch('http://localhost:8000/blog/' + id, {
-            method: 'DELETE',
-        });
-        if (!response.ok)
-            throw new Error(`HTTP error status: ${response.status}`)
-    }
-
-    const handleDeleteClick = (id) => {
-        deleteData(id)
-            .catch(e => <div><h3>{e.message}</h3></div>)
-    }
-    */
-
     const handleDeleteClick = (id) => {
         fetch('http://localhost:8000/blog/' + id, {
             method: 'DELETE',
--- src/components/blog/EntryDetail.jsx
+++ src/components/blog/EntryDetail.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useHistory, useParams} from 'react-router-dom';
 import useFetch from "../../hooks/useFetch";
 
 const EntryDetail = () => {
     const {id} = useParams();
     const {data: entry, fetchError, isPending} = useFetch('http://localhost:8000/blog/' + id);
+    const history = useHistory();
+
+    const deleteData = async () => {
+        const response = await fetch('http://localhost:8000/blog/' + id, {
+            method: 'DELETE',
+        });
+        if (!response.ok)
+            throw new Error(`HTTP error status: ${response.status}`)
+    }
+
+    const handleDeleteClick = () => {
+        deleteData()
+            .catch(e => <div><h3>{e.message}</h3></div>);
+        history.push('/');
+    }
 
     return (
         <div className="entry-details">
@@ -15,6 +30,9 @@ const EntryDetail = () => {
                 <h4>By: {entry.author}</h4>
                 <hr/>
                 <p>{entry.content}</p>
+                <button type="button" className="btn btn-outline-danger btn-sm"
+                        onClick={handleDeleteClick}>Delete
+                </button>
             </div>}
         </div>
     );
