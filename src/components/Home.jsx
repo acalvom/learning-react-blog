96531c5e14c37c04b4240f49e80cc321e2f75246
acalvom <andreac.3108@gmail.com>
2021-06-10 11:50:14 +0200

    Add useSelector hook to get central state (ant: mapStateToProp)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,18 @@
 import React, {Fragment} from 'react';
 import './Home.css'
 import EntriesList from "./blog/EntriesList";
-import useFetch from "../hooks/useFetch";
+import{useSelector} from "react-redux";
+
 
 const Home = () => {
-    const {data: blog, isPending, fetchError} = useFetch('http://localhost:8000/blog');
+    const blog = useSelector(state => state.blog);
 
     return (
         <Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col-sm">
-                        {isPending && <div><h2>Fetching data...</h2></div>}
                         {blog && <EntriesList entries={blog} pageTitle={"Blog Entries"}/>}
-                        {fetchError && <div><h3>{fetchError}</h3></div>}
                     </div>
                 </div>
             </div>
