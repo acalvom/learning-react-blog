393b8db764d319108322a28e40f6cf2650e9f4ac
acalvom <andreac.3108@gmail.com>
2021-06-10 12:48:45 +0200

    Add useSelector hook to get central state from an id item (ant: mapStateToProp) & remove unnecessary code fragments

--- src/components/blog/EntryDetail.jsx
+++ src/components/blog/EntryDetail.jsx
@@ -1,38 +1,18 @@
 import React from 'react';
-import {useHistory, useParams} from 'react-router-dom';
-import useFetch from "../../hooks/useFetch";
+import {useParams} from 'react-router-dom';
+import {useSelector} from "react-redux";
 
 const EntryDetail = () => {
     const {id} = useParams();
-    const {data: entry, fetchError, isPending} = useFetch('http://localhost:8000/blog/' + id);
-    const history = useHistory();
-
-    const deleteData = async () => {
-        const response = await fetch('http://localhost:8000/blog/' + id, {
-            method: 'DELETE',
-        });
-        if (!response.ok)
-            throw new Error(`HTTP error status: ${response.status}`)
-    }
-
-    const handleDeleteClick = () => {
-        deleteData()
-            .catch(e => <div><h3>{e.message}</h3></div>);
-        history.push('/');
-    }
+    const entry = useSelector((state) => state.blog[id]);
 
     return (
         <div className="entry-details">
-            {isPending && <div><h2>Loading entry...</h2></div>}
-            {fetchError && <div><h3>{fetchError}</h3></div>}
             {entry && <div>
                 <h2>{entry.title}</h2>
                 <h4>By: {entry.author}</h4>
                 <hr/>
                 <p>{entry.content}</p>
-                <button type="button" className="btn btn-outline-danger btn-sm"
-                        onClick={handleDeleteClick}>Delete
-                </button>
             </div>}
         </div>
     );
